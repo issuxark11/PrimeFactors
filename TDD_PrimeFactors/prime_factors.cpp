c64e1902d41272bbebabcdaa26b83631ac7dcb15
yj00114.jang <yjj00114@gmail.com>
2025-03-27 15:28:43 +0900

    3 까지 Testcase Pass

--- TDD_PrimeFactors/prime_factors.cpp
+++ TDD_PrimeFactors/prime_factors.cpp
@@ -7,8 +7,8 @@ class PrimeFactor {
 public:
 	vector<int> of(int number) {
 		vector<int> ret = {};
-		if (number == 2) {
-			ret.push_back(2);
+		if (number > 1) {
+			ret.push_back(number);
 		}
 		return ret;
 	}
