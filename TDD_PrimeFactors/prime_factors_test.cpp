ab7f1762c578689c39235f6e1ee443cd4c9eb0f0
yj00114.jang <yjj00114@gmail.com>
2025-03-27 15:27:00 +0900

    test case refectoring

--- TDD_PrimeFactors/prime_factors_test.cpp
+++ TDD_PrimeFactors/prime_factors_test.cpp
@@ -3,19 +3,19 @@
 
 using namespace std;
 
-TEST(PrimeFactors, PrimeTest) {
-	EXPECT_EQ(1, 1);
-}
-
-TEST(PrimeFactors, PrimeTest1) {
+class PrimeFixture : public testing::Test {
+public:
 	PrimeFactor prime_factor;
-	vector<int> expected = {};
+	vector<int> expected;
+};
+
+TEST_F(PrimeFixture, Of1) {
+	expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
 }
 
-TEST(PrimeFactors, PrimeTest2) {
-	PrimeFactor prime_factor;
-	vector<int> expected = { 2 };
+TEST_F(PrimeFixture, Of2) {
+	expected = { 2 };
 	EXPECT_EQ(expected, prime_factor.of(2));
 }
 
