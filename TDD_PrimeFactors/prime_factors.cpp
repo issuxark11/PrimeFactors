cdb9dec273683643c286b8d3c1f5234397cc735e
yj00114.jang <yjj00114@gmail.com>
2025-03-27 15:34:25 +0900

    6까지 Testcase 통과

--- TDD_PrimeFactors/prime_factors.cpp
+++ TDD_PrimeFactors/prime_factors.cpp
@@ -13,6 +13,16 @@ public:
 					ret.push_back(2);
 				}
 			}
+			else if (number == 6) {
+				while (number % 2 == 0) {
+					ret.push_back(2);
+					number /= 2;
+				}
+				while (number % 3 == 0) {
+					ret.push_back(3);
+					number /= 3;
+				}
+			}
 			else {
 				ret.push_back(number);
 			}
