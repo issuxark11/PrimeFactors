8b63ae0b4a40b088f6eb55ac54b38424433b880f
yj00114.jang <yjj00114@gmail.com>
2025-03-27 15:38:05 +0900

    refectoring

--- TDD_PrimeFactors/prime_factors.cpp
+++ TDD_PrimeFactors/prime_factors.cpp
@@ -7,26 +7,15 @@ class PrimeFactor {
 public:
 	vector<int> of(int number) {
 		vector<int> ret = {};
-		if (number > 1) {
-			if (number == 4) {
-				while (number % 2 == 0) {
-					ret.push_back(2);
-				}
-			}
-			else if (number == 6) {
-				while (number % 2 == 0) {
-					ret.push_back(2);
-					number /= 2;
-				}
-				while (number % 3 == 0) {
-					ret.push_back(3);
-					number /= 3;
-				}
-			}
-			else {
-				ret.push_back(number);
+		
+		for (int divisor = 2; number > 1; divisor++)
+		{
+			while (number % divisor == 0) {
+				ret.push_back(divisor);
+				number /= divisor;
 			}
 		}
+
 		return ret;
 	}
 };
