bb290b91102b22332a4fdc15b55b4b675abd5038
yj00114.jang <yjj00114@gmail.com>
2025-03-27 15:23:01 +0900

    2까지 Test Case Pass

--- TDD_PrimeFactors/prime_factors.cpp
+++ TDD_PrimeFactors/prime_factors.cpp
@@ -1,1 +1,15 @@
-﻿
+﻿#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class PrimeFactor {
+public:
+	vector<int> of(int number) {
+		vector<int> ret = {};
+		if (number == 2) {
+			ret.push_back(2);
+		}
+		return ret;
+	}
+};
--- TDD_PrimeFactors/prime_factors_test.cpp
+++ TDD_PrimeFactors/prime_factors_test.cpp
@@ -1,9 +1,22 @@
 #include "gmock/gmock.h"
+#include "prime_factors.cpp"
 
 using namespace std;
 
 TEST(PrimeFactors, PrimeTest) {
 	EXPECT_EQ(1, 1);
+}
+
+TEST(PrimeFactors, PrimeTest1) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {};
+	EXPECT_EQ(expected, prime_factor.of(1));
+}
+
+TEST(PrimeFactors, PrimeTest2) {
+	PrimeFactor prime_factor;
+	vector<int> expected = { 2 };
+	EXPECT_EQ(expected, prime_factor.of(2));
 }
 
 int main() {
